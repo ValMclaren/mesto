49bb832d526c124edd387bf37a8d28121b6a84c4
ValMclaren <val.vashurina@gmail.com>
2021-10-11 02:22:33 +0300

    after review 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,6 @@
-let modalWindow = document.querySelector('.popup');
-let modalWindowCloseBtn = modalWindow.querySelector('.popup__close');
+let popup = document.querySelector('.popup');
+let popupCloseBtn = popup.querySelector('.popup__close');
 let editProfileBtn = document.querySelector('.profile__edit-btn');
-
-function toggleModalWindow() {
-  modalWindow.classList.toggle('popup_is-opened');
-}
-
-editProfileBtn.addEventListener('click', toggleModalWindow);
-modalWindowCloseBtn.addEventListener('click', toggleModalWindow);
-
 
 let formElement = document.querySelector('.popup');
 let nameInput = document.querySelector('.popup__field_type_name');
@@ -19,19 +11,25 @@ let profileJob = document.querySelector('.profile__caption');
 let saveBtn = document.querySelector('.popup__save');
 let form = document.querySelector('.popup__form');
 
+function openPopup() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popup.classList.toggle('popup_is-opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_is-opened');
+}
+
+editProfileBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value
   profileJob.textContent = jobInput.value
-  saveBtn.addEventListener('click', toggleModalWindow)
+  closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-function formProfile () {
-  nameInput.value = profileName.textContent
-  jobInput.value = profileJob.textContent
-}
-
-editProfileBtn.addEventListener('click', formProfile);
