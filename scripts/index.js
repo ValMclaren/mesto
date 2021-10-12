a6e12c9eda1f7012708e81fb0737f636814c140a
ValMclaren <val.vashurina@gmail.com>
2021-10-12 15:10:44 +0300

    after review 2

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,22 @@ let popup = document.querySelector('.popup');
 let popupCloseBtn = popup.querySelector('.popup__close');
 let editProfileBtn = document.querySelector('.profile__edit-btn');
 
-let formElement = document.querySelector('.popup');
 let nameInput = document.querySelector('.popup__field_type_name');
 let jobInput = document.querySelector('.popup__field_type_job');
 
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__caption');
-let saveBtn = document.querySelector('.popup__save');
 let form = document.querySelector('.popup__form');
 
 function openPopup() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-  popup.classList.toggle('popup_is-opened');
+  popup.classList.add('popup_is-opened');
 }
 
 function closePopup() {
   popup.classList.remove('popup_is-opened');
 }
-
-editProfileBtn.addEventListener('click', openPopup);
-popupCloseBtn.addEventListener('click', closePopup);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -31,5 +26,7 @@ function formSubmitHandler (evt) {
   closePopup();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+editProfileBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
+popup.addEventListener('submit', formSubmitHandler);
 
